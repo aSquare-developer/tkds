2927cf390c3ca7bbc3aadc8363ae1864ba81958a
aSquare <artur.anissimov@gmail.com>
2022-07-31 19:10:57 +0300

    Add two button in a header

--- resources/views/pages/sections/header.blade.php
+++ resources/views/pages/sections/header.blade.php
@@ -13,7 +13,9 @@
     <div class="d-flex h-100 text-center align-items-center">
       <div class="w-100 text-white">
         <h1 class="display-3">TKDS Dance Studio</h1>
-        <p class="lead mb-0">Tantsukool kaasaegsete tantsustiilidega, mis sobib inimestele igas vanuserühmas ja erineva tasemega. </p>
+        <p class="lead mb-3">Tantsukool kaasaegsete tantsustiilidega, mis sobib inimestele igas vanuserühmas ja erineva tasemega. </p>
+        <a href="#" class="btn btn-outline-success btn-lg">VÕTA PROVITRENNI </a>
+        <a href="#" class="btn btn-outline-success btn-lg">REGISTREETI TUNDIDESSE</a>
       </div>
     </div>
   </div>

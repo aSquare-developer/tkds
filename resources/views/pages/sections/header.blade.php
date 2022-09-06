e9ccfccd17225aa12f40c5b76cec4be445ea9e82
aSquare <artur.anissimov@gmail.com>
2022-09-06 21:41:03 +0300

    Some visual bug fix

--- resources/views/pages/sections/header.blade.php
+++ resources/views/pages/sections/header.blade.php
@@ -14,8 +14,8 @@
       <div class="w-100 text-white">
         <h1 class="display-3">TKDS Dance Studio</h1>
         <p class="lead mb-4">Tantsustuudio kaasaegsete tantsustiilidega, mis sobib inimestele igas vanuserühmas ja erineva tasemega. </p>
-        <a href="#" class="btn btn-success btn-lg" data-toggle="modal" data-target=".trial-training-modal-lg">VÕTA PROVITRENNI</a>
-        <a href="#" class="btn btn-success btn-lg" data-toggle="modal" data-target=".register-modal-lg">REGISTREERI TUNDIDESSE</a>
+        <a href="#" class="btn btn-success btn-lg mt-3" data-toggle="modal" data-target=".register-modal-lg">REGISTREERI TUNDIDESSE</a>
+        <a href="#" class="btn btn-success btn-lg mt-3" data-toggle="modal" data-target=".trial-training-modal-lg">PROVITRENN</a>
       </div>
     </div>
   </div>

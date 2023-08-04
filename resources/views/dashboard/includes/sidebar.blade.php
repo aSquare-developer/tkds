eb90f73e5fea31719fa5fc9180fad37d413b2b51
aSquare <artur.anissimov@gmail.com>
2023-08-04 23:23:53 +0300

    Dashboard Create new Teacher form image input required some bug fix and improvements

--- resources/views/dashboard/includes/sidebar.blade.php
+++ resources/views/dashboard/includes/sidebar.blade.php
@@ -28,10 +28,6 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
                                 </svg>
                                 Requests
-{{--                                @if(isset($requestsCount))--}}
-{{--                                    <span class="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">{{ $requestsCount }}</span>--}}
-{{--                                @endif--}}
-
                             </a>
                         </li>
                         <li>

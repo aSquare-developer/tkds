486cc1baa6a20fee58044390b3044a0cee853017
Artur-MacBook-Air <artur@example.com>
2024-08-08 06:42:41 +0300

    Dashboard index page modify

--- resources/views/dashboard/pages/index.blade.php
+++ resources/views/dashboard/pages/index.blade.php
@@ -8,14 +8,10 @@
 @section('content')
     <div class="bg-white">
         <div class="mx-auto max-w-7xl space-y-8">
-            <dl class="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+            <dl class="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-2">
                 <div class="mx-auto flex max-w-xs flex-col gap-y-4 border rounded-2xl p-10">
                     <dt class="text-base leading-7 text-gray-600">Requests</dt>
                     <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">{{ $requestsCount }}</dd>
-                </div>
-                <div class="mx-auto flex max-w-xs flex-col gap-y-4 border rounded-2xl p-10">
-                    <dt class="text-base leading-7 text-gray-600">Tasks</dt>
-                    <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">{{ $tasksCount }}</dd>
                 </div>
                 <div class="mx-auto flex max-w-xs flex-col gap-y-4 border rounded-2xl p-10">
                     <dt class="text-base leading-7 text-gray-600">Lessons</dt>
